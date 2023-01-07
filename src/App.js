78fb9ea83622e5d590cd73a99f82008f339ffae9
Dilip <dilippoudel578@gmail.com>
2023-01-07 12:18:32 +0200

    Extracted components and its state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
+import BlogPostForm from './components/BlogForm/BlogForm'
 import Notification from './components/Notification/Notification'
 import blogService from './services/blogs'
 import loginService from './services/login'
@@ -9,9 +10,6 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [url, setUrl] = useState('')
-  const [author, setAuthor] = useState('')
-  const [title, setTitle] = useState('')
   const [notification, setNotification] = useState(null)
   useEffect(() => {
     const loggedUserJSON = localStorage.getItem('loggedBlogappUser')
@@ -56,8 +54,7 @@ const App = () => {
     }
   }
 
-  const blogSubmitHandle = async (e) => {
-    e.preventDefault()
+  const blogSubmitHandle = async (url, author, title) => {
     try {
       await blogService.create({ url, author, title })
       const newMessage = {
@@ -66,10 +63,6 @@ const App = () => {
       }
       setNotification(newMessage)
       setTimeout(() => setNotification(null), 3000)
-
-      setAuthor('')
-      setUrl('')
-      setTitle('')
     } catch (exception) {
       const newMessage = {
         ...notification,
@@ -79,39 +72,6 @@ const App = () => {
       setTimeout(() => setNotification(null), 3000)
     }
   }
-
-  const blogPostForm = () => (
-    <div>
-      <h2>Create new</h2>
-      <form onSubmit={blogSubmitHandle}>
-        <label htmlFor="title">Title</label>
-        <input
-          type="text"
-          name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <br />
-        <label htmlFor="author">Author</label>
-        <input
-          type="text"
-          name="author"
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-        <br />
-        <label htmlFor="url">url</label>
-        <input
-          type="text"
-          name="url"
-          value={url}
-          onChange={(e) => setUrl(e.target.value)}
-        />
-        <br />
-        <button type="submit">Create</button>
-      </form>
-    </div>
-  )
 
   return (
     <div>
@@ -142,7 +102,7 @@ const App = () => {
         </div>
       ) : (
         <div>
-          {blogPostForm()}
+          <BlogPostForm onSubmit={blogSubmitHandle} />
           <h2>blogs</h2>
           <p>{user.name} is logged in</p>
           <button
---
+++ src/components/BlogForm/BlogForm.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+const BlogPostForm = ({ blogSubmitHandle }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
+  const addBlog = (e) => {
+    e.preventDefault()
+    blogSubmitHandle(url, author, title)
+  }
+  return (
+    <div>
+      <h2>Create new</h2>
+      <form onSubmit={addBlog}>
+        <label htmlFor="title">Title</label>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <br />
+        <label htmlFor="author">Author</label>
+        <input
+          type="text"
+          name="author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
+        <br />
+        <label htmlFor="url">url</label>
+        <input
+          type="text"
+          name="url"
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+        />
+        <br />
+        <button type="submit">Create</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogPostForm
