f34c0bae29f290f30bf63605db253dd446252854
Dilip <dilippoudel578@gmail.com>
2023-01-07 13:56:01 +0200

    Add stylefor blogpost and hide show functionality

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -1,7 +1,33 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import './Blog.css'
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+  const buttonText = visible ? 'hide' : 'show'
+
+  return (
+    <div className="blog_container">
+      <div className="blog_title">
+        <p className="blog_title_text">
+          {blog.title} <button onClick={toggleVisibility}>{buttonText}</button>
+        </p>
+      </div>
+      <div style={showWhenVisible}>
+        <p>
+          <a href="#">{blog.url}</a>
+        </p>
+        <p>
+          Likes: {blog.likes} <button>Like</button>
+        </p>
+        <p>
+          <b>{blog.author}</b>{' '}
+        </p>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
